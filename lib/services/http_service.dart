87eeeded519231f481caf0c312d966ca6cec7182
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-14 22:49:20 +0530

    Understanding Dio HTTP package

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -14,4 +14,15 @@ class HTTPService {
     _appConfig = GetIt.instance.get<AppConfig>();
     _base_url = _appConfig!.COIN_API_BASE_URL;
   }
+
+  Future<Response?> get(String _path) async {
+    try {
+      String _url = "$_base_url$_path";
+      Response _response = await dio.get(_url);
+      print(_response);
+    } catch (e) {
+      print("HTTPService: Unable to perform get request. ");
+      print(e);
+    }
+  }
 }
