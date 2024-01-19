d7e368ddae1c48083e5a49b1e86b03cb1486b16f
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-19 22:07:30 +0530

    Displaying Coin Image

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -99,6 +99,9 @@ class _HomePageState extends State<HomePage> {
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              _coinImageWidget(
+                _data["image"]["large"],
+              ),
               _currentPriceWidget(_usdPrice),
               _percentageChangeWidget(_change24h),
             ],
@@ -132,6 +135,21 @@ class _HomePageState extends State<HomePage> {
         color: Colors.white,
         fontSize: 15,
         fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _coinImageWidget(String _imgURL) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: _deviceHeight! * 0.02,
+      ),
+      height: _deviceHeight! * 0.15,
+      width: _deviceWidth! * 0.15,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(_imgURL),
+        ),
       ),
     );
   }
